36520e73c7164b6c73a8ec121706ca57f6a9fad1
Piyush-debug53 <piyushbhatia1211@gmail.com>
2026-03-31 23:18:55 +0530

    Added FPS script in inference.py

--- src/inference.py
+++ src/inference.py
@@ -1,3 +1,97 @@
+# import torch
+# import torchvision
+# import cv2
+# import os
+# import time
+
+# # ----------------------------
+# # Config
+# # ----------------------------
+# MODEL_PATH = "animal_detector_final.pth"
+# VAL_IMAGES_PATH = "../data/val/images"
+
+# NUM_IMAGES = 100     # ✅ same as YOLO
+# WARMUP_RUNS = 5      # ✅ remove cold start effect
+
+# # ----------------------------
+# # Device
+# # ----------------------------
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# print("Using device:", device)
+
+# # ----------------------------
+# # Load Model
+# # ----------------------------
+# model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=None)
+
+# num_classes = 6
+# in_features = model.roi_heads.box_predictor.cls_score.in_features
+
+# model.roi_heads.box_predictor = torchvision.models.detection.faster_rcnn.FastRCNNPredictor(
+#     in_features,
+#     num_classes
+# )
+
+# model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
+# model.to(device)
+# model.eval()
+
+# print("Model loaded!")
+
+# # ----------------------------
+# # Load Images (Preload)
+# # ----------------------------
+# image_files = [
+#     f for f in os.listdir(VAL_IMAGES_PATH)
+#     if f.lower().endswith((".jpg", ".png", ".jpeg"))
+# ][:NUM_IMAGES]
+
+# frames = []
+# for img_name in image_files:
+#     img_path = os.path.join(VAL_IMAGES_PATH, img_name)
+#     image = cv2.imread(img_path)
+#     if image is not None:
+#         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#         tensor = torchvision.transforms.ToTensor()(image_rgb).to(device)
+#         frames.append(tensor)
+
+# print(f"Loaded {len(frames)} images")
+
+# # ----------------------------
+# # Warm-up (VERY IMPORTANT)
+# # ----------------------------
+# with torch.no_grad():
+#     for _ in range(WARMUP_RUNS):
+#         _ = model([frames[0]])
+
+# print("Warm-up completed")
+
+# # ----------------------------
+# # FPS Calculation
+# # ----------------------------
+# total_time = 0
+
+# with torch.no_grad():
+#     for img_tensor in frames:
+
+#         start_time = time.time()
+
+#         _ = model([img_tensor])
+
+#         if device.type == "cuda":
+#             torch.cuda.synchronize()
+
+#         end_time = time.time()
+
+#         total_time += (end_time - start_time)
+
+# # ----------------------------
+# # Final FPS
+# # ----------------------------
+# avg_fps = len(frames) / total_time
+
+# print(f"\n🔥 Average FPS (Faster R-CNN): {avg_fps:.2f}")
+
 import torch
 import torchvision
 import cv2
